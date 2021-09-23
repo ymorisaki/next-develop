c9da19c0ba9ee00b17bb45049d100251e5070e8d
MorisakiYuji <youkourow@yahoo.co.jp>
2021-09-23 15:58:21 +0900

    test

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { Swiper } from 'swiper/react';
 import 'swiper/css';
 
-const Slider = ({ children } : { children: JSX.Element[] }) : JSX.Element => (
-  <Swiper
-    spaceBetween={50}
-    slidesPerView={3}
-    loop
-    onSlideChange={() => console.log('slide change')}
-    onSwiper={(swiper) => console.log(swiper)}
-  >
-    {children}
-  </Swiper>
+const Slider = ({
+  children,
+  spaceBetween = 0,
+} : {
+    children: JSX.Element[],
+    spaceBetween: number
+    }) : JSX.Element => (
+      <Swiper
+        spaceBetween={spaceBetween}
+        slidesPerView={3}
+        loop
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {children}
+      </Swiper>
 );
 
 export default Slider;
